5e30ff1b68d521caae802caee28b96d8f9dc5bdb
Taher Maimoon <tmaimoon18@gmail.com>
2024-08-15 23:28:34 +0530

    install Convex and setup project

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -6,9 +6,12 @@ import { ArrowRight } from "lucide-react";
 const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
-            <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">Your Ideas, Documents, & Plans - Unified. Welcome to <span className="underline">Notations</span>
+            <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
+                Your Ideas, Documents, & Plans - Unified. Welcome to <span className="underline">Notations</span>
             </h1>
-            <h3 className="text-base sm:texx-xl md:text-2xl font-medium">Notations is the connected workspace where <br/> better, faster work happens</h3>
+            <h3 className="text-base sm:texx-xl md:text-2xl font-medium">
+                Notations is the connected workspace where <br/> better, faster work happens
+            </h3>
             <Button>Enter Notations <ArrowRight className="h-4 w-4 ml-2"/></Button>
         </div>
     );
--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -1,17 +1,40 @@
 import Image from "next/image";
+
 const Heroes = () => {
     return (
         <div className="flex flex-col items-center justify-center max-w-5xl">
             <div className="flex items-center">
                 <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
-                    <Image src="/documents.webp" fill className="object-contain" alt="Documents"/>
+                    <Image
+                        src="/documents.webp"
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="Documents-Light"
+                    />
+                    <Image
+                        src="/documents-dark.webp"
+                        fill
+                        className="object-contain hidden dark:block"
+                        alt="Documents-Dark"
+                    />
                 </div>
                 <div className="relative w-[400px] h-[400px] hidden md:block">
-                    <Image src="/reading.webp" fill className="object-contain" alt="Reading"/>
+                    <Image
+                        src="/reading.webp"
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="Reading-Light"
+                    />
+                    <Image
+                        src="/reading-dark.webp"
+                        fill
+                        className="object-contain hidden dark:block"
+                        alt="Reading-Dark"
+                    />
                 </div>
             </div>
         </div>
     );
-}
- 
+};
+
 export default Heroes;
