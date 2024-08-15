7f44717e795c3d0c92933268b5c8e840eecf3f43
Taher Maimoon <tmaimoon18@gmail.com>
2024-08-16 00:06:44 +0530

    use ConvexClientProvider in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,50 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-providers'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-providers";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Notations',
-  description: 'A workspace to organize your work better and faster.',
-  icons : {
-    icon : [
-        {
-            media: "(prefers-color-scheme: light)",
-            url: "/logo.svg",
-            href : "/logo.svg",
-        },
-        {
-            media: "(prefers-color-scheme: dark)",
-            url: "/logo-dark.svg",
-            href : "/logo-dark.svg",
-        }
-    ]
-  }
-}
+  title: "Notations",
+  description: "A workspace to organize your work better and faster.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
-            <ThemeProvider
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-            storageKey="notations-theme">
-                {children}
-            </ThemeProvider>
-        </body>
+            storageKey="notations-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
     </html>
-  )
+  );
 }
