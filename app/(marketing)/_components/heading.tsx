9878782ed84a4839dafa50edf3b746a40bdc9fd6
Taher Maimoon <tmaimoon18@gmail.com>
2024-08-16 01:01:34 +0530

    completed authentication with clerk. create Spinner component. handle sign-in or enter with isAuthenticated and loading spinner with isLoading

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
@@ -12,7 +18,27 @@ const Heading = () => {
             <h3 className="text-base sm:texx-xl md:text-2xl font-medium">
                 Notations is the connected workspace where <br/> better, faster work happens
             </h3>
-            <Button>Enter Notations <ArrowRight className="h-4 w-4 ml-2"/></Button>
+            { isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg"/>
+                </div>
+            )}
+            { isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter Notations <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Link>
+                </Button>
+            )}
+            { !isAuthenticated && !isLoading && (
+                <>
+                    <SignInButton mode="modal">
+                        <Button>
+                            Get Notations Free <ArrowRight className="h-4 w-4 ml-2"/>
+                        </Button>
+                    </SignInButton>
+                </>
+            )}
         </div>
     );
 }
