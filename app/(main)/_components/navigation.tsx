be786ac4bbcad0f540b0d78741940623f5166cd4
Taher Maimoon <tmaimoon18@gmail.com>
2024-08-18 00:46:52 +0530

    created reusable Item component in (main) and used in Navigation to create new notes and show documents

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 import { cn } from "@/lib/utils";
 import UserItem from "./user-item";
 import { api } from "@/convex/_generated/api";
+import Item from "./item";
+import { toast } from "sonner";
 
 const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
     const documents = useQuery(api.documents.get);
+    const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -24,8 +27,7 @@ const Navigation = () => {
     useEffect(() => {
         if (isMobile) {
             collapse();
-        }
-        else {
+        } else {
             resetWidth();
         }
     }, [isMobile]);
@@ -46,27 +48,36 @@ const Navigation = () => {
 
         document.addEventListener("mousemove", handleMouseMove);
         document.addEventListener("mouseup", handleMouseUp);
-    }
+    };
 
     const handleMouseMove = (event: MouseEvent) => {
-        if (!isResizingRef) { return; }
+        if (!isResizingRef) {
+            return;
+        }
         let newWidth = event.clientX;
 
-        if (newWidth < 240) { newWidth = 240; }
-        if (newWidth > 480) { newWidth = 480; }
+        if (newWidth < 240) {
+            newWidth = 240;
+        }
+        if (newWidth > 480) {
+            newWidth = 480;
+        }
 
         if (sidebarRef.current && navbarRef.current) {
             sidebarRef.current.style.width = `${newWidth}px`;
             navbarRef.current.style.setProperty("left", `${newWidth}px`);
-            navbarRef.current.style.setProperty("width", `clac(100% - ${newWidth}px)`);
+            navbarRef.current.style.setProperty(
+                "width",
+                `clac(100% - ${newWidth}px)`
+            );
         }
-    }
+    };
 
     const handleMouseUp = () => {
         isResizingRef.current = false;
         document.removeEventListener("mousemove", handleMouseMove);
         document.removeEventListener("mouseup", handleMouseUp);
-    }
+    };
 
     const resetWidth = () => {
         if (sidebarRef.current && navbarRef.current) {
@@ -74,11 +85,19 @@ const Navigation = () => {
             setIsResetting(true);
 
             sidebarRef.current.style.width = isMobile ? "100%" : "240px";
-            navbarRef.current.style.setProperty("width", isMobile ? "0" : "calc(100% - 240px)");
-            navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
-            setTimeout(() => { setIsResetting(false); }, 300);
+            navbarRef.current.style.setProperty(
+                "width",
+                isMobile ? "0" : "calc(100% - 240px)"
+            );
+            navbarRef.current.style.setProperty(
+                "left",
+                isMobile ? "100%" : "240px"
+            );
+            setTimeout(() => {
+                setIsResetting(false);
+            }, 300);
         }
-    }
+    };
 
     const collapse = () => {
         if (sidebarRef.current && navbarRef.current) {
@@ -89,8 +108,19 @@ const Navigation = () => {
             navbarRef.current.style.setProperty("width", "100%");
             navbarRef.current.style.setProperty("left", "0");
 
-            setTimeout(() => { setIsResetting(false) }, 300);
+            setTimeout(() => {
+                setIsResetting(false);
+            }, 300);
         }
+    };
+
+    const handleCreate = () => {
+        const promise = create({ title: "Untitled" });
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note."
+        });
     }
 
     return (
@@ -107,30 +137,32 @@ const Navigation = () => {
                     role="button"
                     onClick={collapse}
                     className={cn(
-                        "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dar:hover:bg-neutral-600 absolute top-3 right-2 oopacity-0 group-hover/sidebar:opacity-100 transition",
+                        "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 oopacity-0 group-hover/sidebar:opacity-100 transition",
                         isMobile && "opacity-100"
                     )}
                 >
                     <ChevronsLeft></ChevronsLeft>
                 </div>
-                    {/* Action Items */}
+                {/* Action Items */}
                 <div>
                     <UserItem />
+                    <Item
+                        onClick={handleCreate}
+                        label="New Page"
+                        icon={PlusCircle}
+                    />
                 </div>
-                    {/* Documents */}
+                {/* Documents */}
                 <div className="mt-4">
-                    {
-                        documents?.map((document) => (
-                            <p key={document._id}>
-                                {document.title}
-                            </p>
-                        ))
-                    }
+                    {documents?.map((document) => (
+                        <p key={document._id}>{document.title}</p>
+                    ))}
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
                     onClick={resetWidth}
-                    className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+                    className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+                />
             </aside>
 
             <div
