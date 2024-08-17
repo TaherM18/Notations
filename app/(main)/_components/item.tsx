23698d9ce3ce1c05592c8d0bacf53e8cd67c7925
Taher Maimoon <tmaimoon18@gmail.com>
2024-08-18 01:42:44 +0530

    created items for search and settings in sidebar using Item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -35,7 +35,7 @@ const Item = ({
         <div
             role="button"
             onClick={onClick}
-            style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+            style={{ paddingLeft: level ? `${(level * 12) + 12}px` : "12px" }}
             className={cn(
                 "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
                 active && "bg-primary/5 text-primary"
@@ -43,6 +43,7 @@ const Item = ({
         >
             { !!id && (
                 <div
+                    role="button"
                     className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
                     onClick={() => {}}
                 >
@@ -50,14 +51,20 @@ const Item = ({
                 </div>
             )}
             { documentIcon ? (
-                <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+                <div className="shrink-0 mr-2 text-[18px]">
+                    {documentIcon}
+                </div>
             ) : (
-                <Icon className="shrink-0 h-[18px] text-muted-foreground" />
+                <Icon
+                    className="shrink-0 h-[18px] mr-2 text-muted-foreground" 
+                />
             )}
-            <span className="truncate">{label}</span>
+            <span className="truncate">
+                {label}
+            </span>
             { isSearch && (
                 <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-                    <span className="text-xs">CTRL</span>
+                    <span className="text-xs">Ctrl</span>K
                 </kbd>
             )}
         </div>
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -146,6 +146,17 @@ const Navigation = () => {
                 {/* Action Items */}
                 <div>
                     <UserItem />
+                    <Item
+                        label="Search"
+                        icon={Search}
+                        isSearch
+                        onClick={()=>{}}
+                    />
+                    <Item
+                        label="Settings"
+                        icon={Settings}
+                        onClick={()=>{}}
+                    />
                     <Item
                         onClick={handleCreate}
                         label="New Page"
