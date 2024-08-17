032ed7e0055d0d1a28672f511c9e168c59beeb74
Taher Maimoon <tmaimoon18@gmail.com>
2024-08-17 17:04:18 +0530

    set sidebar css, refs, and states

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import { ChevronsLeft } from "lucide-react";
+import { ElementRef, useRef, useState } from "react";
+
 const Navigation = () => {
+    const isResizingRef = useRef(false);
+    const sidebarRef = useRef<ElementRef<"aside">>(null);
+    const navbarRef = useRef<ElementRef<"div">>(null);
+    const [isResetting, setResetting] = useState(false);
+    const [isCollapsed, setCollapsed] = useState(false);
+
     return (
-        <nav>
-            
-        </nav>
+        <>
+            <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+                <div role="button" className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dar:hover:bg-neutral-600 absolute top-3 right-2 oopacity-0 group-hover/sidebar:opacity-100 transition">
+                    <ChevronsLeft></ChevronsLeft>
+                </div>
+                <div>
+                    <p>Action Items</p>
+                </div>
+                <div className="mt-4">
+                    <p>Documents</p>
+                </div>
+                <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+            </aside>
+        </>
     );
-}
- 
+};
+
 export default Navigation;
