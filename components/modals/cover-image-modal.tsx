6c91f962380e5d7866c573122a9151f23c421884
Taher Maimoon <tmaimoon18@gmail.com>
2024-09-17 01:10:32 +0530

    file upload functionality complete

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -1,15 +1,54 @@
 "use client";
 
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { useParams } from "next/navigation";
+
 import {
     Dialog,
     DialogContent,
     DialogHeader
 } from "@/components/ui/dialog";
-
 import { useCoverImage } from "@/hooks/use-cover-image";
+import { SingleImageDropzone } from "@/components/single-image-dropzone";
+import { useEdgeStore } from "@/lib/edgestore";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
 
 const CoverImageModal = () => {
+    const params = useParams();
+    const update = useMutation(api.documents.update);
+    const [file, setFile] = useState<File>()
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
     const coverImage = useCoverImage();
+    const { edgestore } = useEdgeStore();
+
+    const onClose = () => {
+        setFile(undefined);
+        setIsSubmitting(false);
+        coverImage.onClose();
+    }
+
+    const onChange = async (file?: File) => {
+        if (file) {
+            setIsSubmitting(true);
+            setFile(file);
+
+            const res = await edgestore.publicFiles.upload({
+                file
+            });
+
+            await update({
+                id: params.documentId as Id<"documents">,
+                coverImage: res.url
+            });
+
+            onClose();
+        }
+    }
+
     return (
         <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
             <DialogContent>
@@ -18,9 +57,12 @@ const CoverImageModal = () => {
                         Cover Image
                     </h2>
                 </DialogHeader>
-                <div>
-                    TODO: upload image
-                </div>
+                <SingleImageDropzone
+                    className="w-full outline-none"
+                    disabled={isSubmitting}
+                    value={file}
+                    onChange={onChange}
+                />
             </DialogContent>
         </Dialog>
     );
